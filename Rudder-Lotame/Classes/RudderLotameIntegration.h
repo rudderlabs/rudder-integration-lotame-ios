1738c735db4ff04c506c573f28077b2cd6b941b6
Satheesh Kannan <satheesh@rudderstack.com>
2026-03-05 18:32:34 +0530

    feat: update class declaration

--- Rudder-Lotame/Classes/RudderLotameIntegration.h
+++ Rudder-Lotame/Classes/RudderLotameIntegration.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface RudderLotameIntegration : NSObject<RSIntegration>
+@interface RudderLotameIntegration: NSObject<RSIntegration>
 
 @property (nonatomic) BOOL sendEvents;
 
